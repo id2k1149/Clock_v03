ed6d9dd678e8db799358ea84b2e37f3d8f60dbb1
id2k1149 <id2k1149@gmail.com>
2023-04-01 18:16:17 -0400

    Initial Commit

---
+++ Clock_v03/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Clock_v03
+//
+//  Created by Max Franz Immelmann on 4/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
