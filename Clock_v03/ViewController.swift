c694447ca3bef647a947697dafd0c81177d5f711
id2k1149 <id2k1149@gmail.com>
2023-04-02 15:56:38 -0400

    add minutes marks

--- Clock_v03/ViewController.swift
+++ Clock_v03/ViewController.swift
@@ -18,7 +18,8 @@ class ViewController: UIViewController {
         squareView.layer.cornerRadius = squareView.frame.width / 4
         circleView.layer.cornerRadius = circleView.frame.width / 2
         
-        setHours(circleView: circleView)
+        setHours(to: circleView)
+        addMinuteMarks(to: circleView)
 
     }
     
@@ -42,13 +43,11 @@ class ViewController: UIViewController {
 }
 
 extension UIViewController {
-    func setHours(circleView: UIView) {
+    func setHours(to circleView: UIView) {
         
         let labelSize = CGSize(width: 50, height: 50)
-        let circleRadius = circleView.frame.width/2 - labelSize.width/2
-        let circleCenter = CGPoint(x: view.frame.width/2,
-                                   y: view.frame.height/2)
-
+        let circleRadius = circleView.frame.width/2 - labelSize.width/2 - 10
+                                 
         for i in 0..<12 {
             let label = UILabel(frame: CGRect(origin: .zero, size: labelSize))
             let angle = CGFloat(i) / 12.0 * 2.0 * CGFloat.pi - CGFloat.pi/2.0
@@ -56,16 +55,53 @@ extension UIViewController {
             let y = sin(angle) * circleRadius + circleView.frame.height/2
             label.center = CGPoint(x: x, y: y)
             label.text = "\(i == 0 ? 12 : i)"
+            label.font = UIFont.boldSystemFont(ofSize: 30)
             label.textAlignment = .center
             circleView.addSubview(label)
         }
-
-        
-
-
-        
-        
-        
     }
+    
+    /*
+    func addMinuteMarks(to circleView: UIView) {
+        let markSize = CGSize(width: 4, height: 4)
+        let markRadius = circleView.frame.width/2 - markSize.width/2 - 5
+        
+        for i in 0..<60 {
+            let mark = UIView(frame: CGRect(origin: .zero, size: markSize))
+            let angle = CGFloat(i) / 60.0 * 2.0 * CGFloat.pi - CGFloat.pi/2.0
+            let x = cos(angle) * markRadius + circleView.frame.width/2
+            let y = sin(angle) * markRadius + circleView.frame.height/2
+            mark.center = CGPoint(x: x, y: y)
+            mark.backgroundColor = i % 5 == 0 ? UIColor.red : UIColor.black
+            mark.layer.cornerRadius = markSize.width/2
+            circleView.addSubview(mark)
+        }
+    }
+     */
+    
+    func addMinuteMarks(to circleView: UIView) {
+        let markSize = CGSize(width: 8, height: 2)
+        let markRadius = circleView.frame.width/2 - markSize.width
+        
+        for i in 0..<60 {
+            let mark = UIView(frame: CGRect(origin: .zero, size: markSize))
+            let angle = CGFloat(i) / 60.0 * 2.0 * CGFloat.pi - CGFloat.pi/2.0
+            let x = cos(angle) * markRadius + circleView.frame.width/2
+            let y = sin(angle) * markRadius + circleView.frame.height/2
+            mark.center = CGPoint(x: x, y: y)
+            mark.backgroundColor = UIColor.black
+            if i % 5 == 0 {
+                mark.frame.size.height = 3
+            }
+            mark.backgroundColor = i % 5 == 0 ? UIColor.red : UIColor.black
+//            mark.layer.cornerRadius = markSize.width/2
+            mark.transform = CGAffineTransform(rotationAngle: angle)
+            circleView.addSubview(mark)
+        }
+    }
+
+
+
+    
 }
 
