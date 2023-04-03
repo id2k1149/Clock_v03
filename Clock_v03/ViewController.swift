7549e8277d64b428dcd1df1d7326177f6f024952
id2k1149 <id2k1149@gmail.com>
2023-04-02 20:48:37 -0400

    refactor seconds hand

--- Clock_v03/ViewController.swift
+++ Clock_v03/ViewController.swift
@@ -23,26 +23,10 @@ class ViewController: UIViewController {
         addHourHand(to: circleView)
         addMinuteHand(to: circleView)
         addSecondHand(to: circleView)
-
-    }
-    
-    
-    @IBAction func startButton(_ sender: UIButton) {
-//        markView.frame.origin.y += 200
         
-        /*
-        UIView.animate(
-            withDuration: 1,
-            delay: 0,
-            options: [.autoreverse, .repeat]) { [unowned self] in
-                markView.frame.origin.x += 20
-                markView.frame.origin.y += 20
-            }
-         */
+        addCenterCircles(to: circleView)
+
     }
-    
-
-
 }
 
 extension UIViewController {
@@ -50,6 +34,14 @@ extension UIViewController {
         
         let labelSize = CGSize(width: 50, height: 50)
         let circleRadius = circleView.frame.width/2 - labelSize.width/2 - 10
+        
+//        let circleCenter = CGPoint(x: view.frame.width/2, y: view.frame.height/2)
+//        let circleView = UIView(frame: CGRect(origin: .zero, size: CGSize(width: circleRadius*2, height: circleRadius*2)))
+//        circleView.center = circleCenter
+//        circleView.layer.cornerRadius = circleRadius
+//        circleView.layer.borderWidth = 2
+//        circleView.layer.borderColor = UIColor.black.cgColor
+//        view.addSubview(circleView)
                                  
         for i in 0..<12 {
             let label = UILabel(frame: CGRect(origin: .zero, size: labelSize))
@@ -63,24 +55,6 @@ extension UIViewController {
             circleView.addSubview(label)
         }
     }
-    
-    /*
-    func addMinuteMarks(to circleView: UIView) {
-        let markSize = CGSize(width: 4, height: 4)
-        let markRadius = circleView.frame.width/2 - markSize.width/2 - 5
-        
-        for i in 0..<60 {
-            let mark = UIView(frame: CGRect(origin: .zero, size: markSize))
-            let angle = CGFloat(i) / 60.0 * 2.0 * CGFloat.pi - CGFloat.pi/2.0
-            let x = cos(angle) * markRadius + circleView.frame.width/2
-            let y = sin(angle) * markRadius + circleView.frame.height/2
-            mark.center = CGPoint(x: x, y: y)
-            mark.backgroundColor = i % 5 == 0 ? UIColor.red : UIColor.black
-            mark.layer.cornerRadius = markSize.width/2
-            circleView.addSubview(mark)
-        }
-    }
-     */
     
     func addMinuteMarks(to circleView: UIView) {
         let markSize = CGSize(width: 8, height: 2)
@@ -129,7 +103,7 @@ extension UIViewController {
         // Create a path for the hour hand
         let hourPath = UIBezierPath()
         hourPath.move(to: centerPoint)
-        let hourAngle = getCurrentAngle(for: .hour, in: .current)
+        let hourAngle = getCurrentAngle(for: .hour)
         let hourEndPoint = CGPoint(x: cos(hourAngle) * hourRadius + centerPoint.x, y: sin(hourAngle) * hourRadius + centerPoint.y)
         hourPath.addLine(to: hourEndPoint)
         
@@ -145,7 +119,7 @@ extension UIViewController {
         
         // Set up a timer to update the hour hand every minute
         let timer = Timer(fire: Date(), interval: 60.0, repeats: true) { [self] (timer) in
-            let hourAngle = getCurrentAngle(for: .hour, in: .current)
+            let hourAngle = getCurrentAngle(for: .hour)
             let hourEndPoint = CGPoint(x: cos(hourAngle) * hourRadius + centerPoint.x, y: sin(hourAngle) * hourRadius + centerPoint.y)
             hourPath.removeAllPoints()
             hourPath.move(to: centerPoint)
@@ -162,7 +136,7 @@ extension UIViewController {
         // Create a path for the minute hand
         let minutePath = UIBezierPath()
         minutePath.move(to: centerPoint)
-        let minuteAngle = getCurrentAngle(for: .minute, in: .current)
+        let minuteAngle = getCurrentAngle(for: .minute)
         let minuteEndPoint = CGPoint(x: cos(minuteAngle) * minuteRadius + centerPoint.x, y: sin(minuteAngle) * minuteRadius + centerPoint.y)
         minutePath.addLine(to: minuteEndPoint)
         
@@ -178,7 +152,7 @@ extension UIViewController {
         
         // Set up a timer to update the minute hand every second
         let timer = Timer(fire: Date(), interval: 1.0, repeats: true) { [self] (timer) in
-            let minuteAngle = getCurrentAngle(for: .minute, in: .current)
+            let minuteAngle = getCurrentAngle(for: .minute)
             let minuteEndPoint = CGPoint(x: cos(minuteAngle) * minuteRadius + centerPoint.x, y: sin(minuteAngle) * minuteRadius + centerPoint.y)
             minutePath.removeAllPoints()
             minutePath.move(to: centerPoint)
@@ -190,12 +164,12 @@ extension UIViewController {
     
     func addSecondHand(to circleView: UIView) {
         let centerPoint = CGPoint(x: circleView.bounds.midX, y: circleView.bounds.midY)
-        let secondRadius = circleView.frame.width/2.5
+        let secondRadius = circleView.frame.width/2.3
         
         // Create a path for the second hand
         let secondPath = UIBezierPath()
         secondPath.move(to: centerPoint)
-        let secondAngle = getCurrentAngle(for: .second, in: .current)
+        let secondAngle = getCurrentAngle(for: .second)
         let secondEndPoint = CGPoint(x: cos(secondAngle) * secondRadius + centerPoint.x,
                                      y: sin(secondAngle) * secondRadius + centerPoint.y)
         secondPath.addLine(to: secondEndPoint)
@@ -212,16 +186,78 @@ extension UIViewController {
         
         // Set up a timer to update the second hand every second
         let timer = Timer(fire: Date(), interval: 1.0, repeats: true) { [self] (timer) in
-            let secondAngle = getCurrentAngle(for: .second, in: .current)
-            let secondEndPoint = CGPoint(x: cos(secondAngle) * secondRadius + centerPoint.x, y: sin(secondAngle) * secondRadius + centerPoint.y)
-            secondPath.removeAllPoints()
-            secondPath.move(to: centerPoint)
-            secondPath.addLine(to: secondEndPoint)
-            secondLayer.path = secondPath.cgPath
+            let secondAngle = getCurrentAngle(for: .second)
+            let secondEndPoint = CGPoint(x: cos(secondAngle) * secondRadius + centerPoint.x,
+                                         y: sin(secondAngle) * secondRadius + centerPoint.y)
+            let newSecondPath = UIBezierPath()
+            newSecondPath.move(to: centerPoint)
+            newSecondPath.addLine(to: secondEndPoint)
+            newSecondPath.move(to: centerPoint)
+            let oppositeEndPoint = CGPoint(x: cos(secondAngle + .pi) * secondRadius/5 + centerPoint.x,
+                                           y: sin(secondAngle + .pi) * secondRadius/5 + centerPoint.y)
+            newSecondPath.addLine(to: oppositeEndPoint)
+            secondLayer.path = newSecondPath.cgPath
         }
         RunLoop.current.add(timer, forMode: .default)
     }
+    
+    func getCurrentAngle(for component: Calendar.Component) -> CGFloat {
+        let calendar = Calendar.current
+        let hour = CGFloat(calendar.component(.hour, from: Date()))
+        let minute = CGFloat(calendar.component(.minute, from: Date()))
+        let second = CGFloat(calendar.component(.second, from: Date()))
+        let nanosecond = CGFloat(calendar.component(.nanosecond, from: Date()))
+        
+        switch component {
+        case .hour:
+            let hourAngle = (hour + minute/60.0) / 12.0 * 2.0 * CGFloat.pi - CGFloat.pi/2.0
+            return hourAngle
+        case .minute:
+            let minuteAngle = (minute + second/60.0) / 30.0 * CGFloat.pi - CGFloat.pi/2.0
+            return minuteAngle
+        default:
+//            let secondAngle = (second + nanosecond/1_000_000_000.0) / 30.0 * CGFloat.pi - CGFloat.pi/2.0
+            let secondProgress = CGFloat(second) + CGFloat(nanosecond) / 1_000_000_000.0
+//            let secondAngle = secondProgress / 60.0 * CGFloat.pi * 2.0 - CGFloat.pi / 2.0
+            let secondAngle = secondProgress / 30.0 * CGFloat.pi - CGFloat.pi / 2.0
+               
+            return secondAngle
+        }
+    }
+    
+    func addCenterCircles(to view: UIView) {
+        
+        let circleRadius = view.frame.width/25
+        let circleCenter = CGPoint(x: view.frame.width/2, y: view.frame.height/2)
 
+        let circleView = UIView(frame: CGRect(origin: .zero,
+                                              size: CGSize(width: circleRadius*2,
+                                                           height: circleRadius*2)))
+        circleView.center = circleCenter
+        circleView.layer.cornerRadius = circleRadius
+        circleView.layer.borderWidth = 3
+        circleView.layer.borderColor = UIColor.black.cgColor
+        view.addSubview(circleView)
+        
+
+        let orangeCircle = UIView(frame: CGRect(origin: .zero,
+                                                size: CGSize(width: circleRadius*2 - 6,
+                                                             height: circleRadius*2 - 6)))
+        orangeCircle.backgroundColor = UIColor.orange
+        orangeCircle.layer.cornerRadius = (circleRadius*2 - 6.0) / 2.0
+        orangeCircle.center = circleCenter
+        view.addSubview(orangeCircle)
+
+        let whiteCircle = UIView(frame: CGRect(origin: .zero,
+                                                size: CGSize(width: circleRadius*2 - 12,
+                                                             height: circleRadius*2 - 12)))
+        whiteCircle.backgroundColor = UIColor.white
+        whiteCircle.layer.cornerRadius = (circleRadius*2 - 12.0) / 2.0
+        whiteCircle.center = circleCenter
+        view.addSubview(whiteCircle)
+    }
+
+        
     /*
     func getCurrentMinuteAngle() -> CGFloat {
         let calendar = Calendar.current
@@ -266,6 +302,7 @@ extension UIViewController {
     }
      */
     
+    /*******
     func getCurrentAngle(for component: Calendar.Component, in calendar: Calendar) -> CGFloat {
         let now = Date()
         let components = calendar.dateComponents([.hour, .minute, .second, .nanosecond], from: now)
@@ -298,4 +335,5 @@ extension UIViewController {
         let offset = 2.0 * CGFloat.pi * ((componentValue / max) / totalValue)
         return angle + offset
     }
+     *//////
 }
